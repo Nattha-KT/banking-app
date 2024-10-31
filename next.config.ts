545411e919e6b50c0003c977bc48a096e633216a
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-10-31 16:55:49 +0700

    Stated project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* For a tip how to silence the warnings. " [webpack.cache.PackFileCacheStrategy]" */
+  webpack: (config) => {
+    config.infrastructureLogging = {
+        level: "error",
+    };
+
+    return config;
+},
 };
 
 export default nextConfig;
