7065833eef5adb166e7207c0ff7ffc6b3574d2e4
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-10-31 20:20:12 +0700

    Create button

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cn } from "@/libs";
+import { ButtonVariantProps, buttonVariants } from "./button.variant";
+
+
+export type ButtonProps = React.ComponentPropsWithoutRef<"button"> & {
+  asChild?: boolean;
+  backgroundColor?: string;
+  loading?: boolean;
+} & ButtonVariantProps;
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
