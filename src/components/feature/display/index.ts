446aabc71a72477dc9aa5775eeacae1b3a22923f
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-11-09 20:16:12 +0700

    Create bank dropdown

--- src/components/feature/display/index.ts
+++ src/components/feature/display/index.ts
@@ -3,3 +3,4 @@ export { default as BalanceBox } from './balance-box';
 export { default as RecentTransactions } from './recentTransactions';
 export { default as BankInfo } from './bank-info';
 export { default as TransactionsTable } from './transactions-table';
+export { default as BankDropdown } from './bank-dropdown';
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,3 +6,4 @@ export * from './sheet';
 export * from './tabs';
 export * from './table';
 export * from './tooltip';
+export * from './select';
